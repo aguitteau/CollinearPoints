2e8578eb3887c343ba8c0678c875c37c53ac115f
dmcgrath19 <45365062+dmcgrath19@users.noreply.github.com>
2018-11-28 08:40:06 -0800

    Update BruteCollinearPoints.java

--- src/BruteCollinearPoints.java
+++ src/BruteCollinearPoints.java
@@ -1,4 +1,57 @@
 
-public class BruteCollinearPoints {
 
+public class CollinearPoints {
+    public class Point implements Comparable<Point> {
+        int px; 
+        int py; 
+        
+   public Point(int x, int y){
+       // constructs the point (x, y)
+       px = x; 
+       py = y;
+   }
+
+   public   void draw(){
+       // draws this point
+   }
+   public   void drawTo(Point that){
+       // draws the line segment from this point to that point
+   }
+   public String toString(){
+       // string representation
+	   
+	   return ""; //PLACEHOLDER
+   }
+   public int compareTo(Point that){
+       // compare two points by y-coordinates, breaking ties by x-coordinates
+	   
+	   return 0; //PLACEHOLDER
+   }
+   public double slopeTo(Point that){
+       // the slope between this point and that point
+	   
+	   return 0.0; //PLACEHOLDER
+   }
+   public Comparator<Point> slopeOrder(){
+       // compare two points by slopes they make with this point
+	   
+   }
+}
+public class BruteCollinearPoints {
+	//Brute force. Write a program BruteCollinearPoints.java that examines 4 points at a time and checks whether they all lie on the same line segment, returning all such line segments. To check whether the 4 points p, q, r, and s are collinear, check whether the three slopes between p and q, between p and r, and between p and s are all equal.
+
+	   public BruteCollinearPoints(Point[] points){
+	       // finds all line segments containing 4 points
+	   }
+	   public int numberOfSegments(){
+	       // the number of line segments
+		   
+		   return 0; //PLACEHOLDER
+	   }
+	   
+	   public LineSegment[] segments(){
+	       // the line segments
+	   }
+	}
+	}
 }
